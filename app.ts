215ac8621f56d62cbacd599bc32bb19b125cef0a
Marcelo González <marcelogm2042@gmail.com>
2026-01-17 20:20:32 -0400

    initial commit

---
+++ app.ts
@@ -0,0 +1,35 @@
+import { serve } from "bun";
+import index from "./web/index.html";
+
+const server = serve({
+    routes: {
+        "/": index,
+    },
+
+    fetch(req, server) {
+        if (server.upgrade(req)) {
+            return;
+        }
+
+        return new Response("Upgraded failed", { status: 500 });
+    },
+
+    websocket: {
+        open(ws) {
+            console.log("Connected");
+            ws.send("Connected");
+        },
+
+        message(ws, message) {
+            console.log(`Message: ${message}`);
+            ws.send(message);
+        },
+
+        close(ws, code, reason) {
+            console.log(`Code: ${code}, For: ${reason}`);
+            ws.send(`Code: ${code}, For: ${reason}`);
+        },
+    },
+});
+
+console.log(`Server running at: ${server.url}`);
---
+++ web/index.ts
@@ -0,0 +1,18 @@
+const socket = new WebSocket("ws://localhost:3000");
+const form = document.getElementById("form");
+const input = document.getElementById("input") as HTMLInputElement;
+const messages = document.getElementById("messages") as HTMLDivElement;
+
+socket.addEventListener("message", (event) => {
+    const message = `<p>${event.data}</p>`;
+    messages.insertAdjacentHTML("beforeend", message);
+});
+
+form?.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (input.value) {
+        socket.send(input.value);
+        input.value = "";
+    }
+});
